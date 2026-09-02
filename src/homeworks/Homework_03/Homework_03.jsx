b661e7cf4a2557b5091ea94e9e0fb025fbf37052
Kasstaneda1 <mihailcub@gmail.com>
2026-09-02 18:01:08 -0400

    Homework_03: Input, LoginForm and styled Button

---
+++ src/homeworks/Homework_03/Homework_03.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import LoginForm from "../../components/LoginForm/LoginForm";
+function Homework_03() {
+  return (
+    <div className="homework_03_wrapper">
+      <LoginForm />
+    </div>
+  );
+}
+export default Homework_03;
